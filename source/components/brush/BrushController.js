dbbb3a73e97881bf31ea85a10dd487614786d204
martijn <email@martijnbrekelmans.com>
2016-08-02 21:10:46 +0200

    added brush-param tweaking ui

--- source/components/brush/BrushController.js
+++ source/components/brush/BrushController.js
@@ -1,11 +1,18 @@
-function BrushController(ctx, cursorModel) {
+function BrushController(ctx, brushModel) {
     var tag_brushSelect = document.querySelector("#brush-select");
+    var tag_brushParamsContainer = document.querySelector(".brush-params");
 
     selectBrush();
     tag_brushSelect.addEventListener("change", selectBrush);
 
     function selectBrush() {
         var newBrush = tag_brushSelect.options[tag_brushSelect.selectedIndex].value;
-        cursorModel.selectBrush(newBrush);
+        brushModel.selectBrush(newBrush);
+
+        var params = brushModel.activeBrush().parameters;
+        console.log(params);
+        var ui = controlsFromJson(params);
+        tag_brushParamsContainer.innerHTML = "";
+        tag_brushParamsContainer.appendChild(ui);
     }
 }
---
+++ source/controlsFromJson.js
@@ -0,0 +1,58 @@
+function controlNum(name, value, object) {
+    var input = document.createElement("input");
+    var label = document.createElement("label");
+    var txt = document.createTextNode(name);
+
+    label.setAttribute("for", name);
+    input.setAttribute("id", name);
+    input.setAttribute("value", value);
+    input.setAttribute("type", "number");
+
+    input.addEventListener("input", function () {
+        object[name] = parseFloat(this.value, 10);
+    });
+
+    label.appendChild(txt);
+    label.appendChild(input);
+
+    return label;
+}
+
+/**
+ * Creates a form with input elements which bind to a given json object
+ *
+ * ex, controlsFromJson({apples: 5, name: "fruits"})
+ *
+ * would generate an HTMLElement somewhat like
+ * <form>
+ *     <label for="apples">apples</label><input id="apples" type="number" value="5"/>
+ *     <label for="name">name</label><input id="name" value="fruits"/>
+ * </form>
+ *
+ * When any of these input elements get changed, the passed json object gets mutated as well.
+ *
+ * @return HTMLElement Append this element with the controls somewhere to your document
+ */
+function controlsFromJson(object) {
+    var form = document.createElement("form");
+
+    Object.keys(object).forEach(function (key) {
+        var value = object[key];
+
+        var element;
+
+        if (value !== null && value !== undefined) {
+            switch (typeof value) {
+                case "number":
+                    element = controlNum(key, value, object);
+                    break;
+                // todo, add other types if necessary
+                // string, bool
+            }
+        }
+
+        form.appendChild(element);
+    });
+
+    return form;
+}
