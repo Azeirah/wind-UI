08f4318f3b0d64f46a47843a71834815cfe79c38
Martijn <tijntje_7@msn.com>
2016-09-09 00:02:13 +0200

    add serialize/deserialize to the mirrormodel

--- source/components/mirror/MirrorModel.js
+++ source/components/mirror/MirrorModel.js
@@ -1,6 +1,9 @@
 function MirrorModel() {
     this.mirrorTypes = {};
     this.selectedMirrorType = "mirror_none";
+    // false = global mirroring (origin lies at middle of screen)
+    // true = local mirror (butterfly-like mirorring)
+    this.selectedMirroringType = false;
 }
 
 MirrorModel.prototype.addMirror = function (name, mirror) {
@@ -15,10 +18,23 @@ MirrorModel.prototype.selectedMirror = function () {
     return this.mirrorTypes[this.selectedMirrorType];
 };
 
+// mirroringtype = local/global mirroring
 MirrorModel.prototype.selectMirroringType = function (mirroringType) {
     this.mirroringType = mirroringType;
 };
 
 MirrorModel.prototype.selectedMirroringType = function () {
     return this.mirroringType;
+};
+
+MirrorModel.prototype.serialize = function () {
+    return JSON.stringify({
+        selectedMirrorType: this.selectedMirrorType,
+        selectedMirroringType: this.selectedMirroringType
+    });
+};
+
+MirrorModel.prototype.loadFromSerialization = function (data) {
+    this.selectMirrorType = data.selectMirrorType;
+    this.selectMirroringType = data.selectedMirroringType;
 };
