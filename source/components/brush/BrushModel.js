80615047d0f3267c8c6114ea15fb9964f1ce6d6b
Martijn <tijntje_7@msn.com>
2016-09-09 00:18:30 +0200

    Add serialize/deserialize to brushModel

--- source/components/brush/BrushModel.js
+++ source/components/brush/BrushModel.js
@@ -18,3 +18,37 @@ BrushModel.prototype.selectBrush = function(name) {
 BrushModel.prototype.activeBrush = function() {
     return this.brushes[this.selectedBrush];
 };
+
+BrushModel.prototype.serialize = function () {
+    // this function picks out the information from brushes we care about;
+    // their name and current parameter values
+    function preSerializeBrush (name, brush) {
+        return {
+            name: name,
+            parameters: brush.parameters
+        };
+    }
+
+    var preSerialization = {
+        selectedBrush: this.selectedBrush,
+        brushes: {}
+    };
+
+    for (var brushName in this.brushes) {
+        preSerialization.brushes[brushName] = preSerializeBrush(brushName, this.brushes[brushName]);
+    }
+
+    return JSON.stringify(preSerialization);
+};
+
+BrushModel.prototype.loadFromSerialization = function (data) {
+    this.selectedBrush = data.selectedBrush;
+    // assume that all brushes in this dataset exist on this.brushes already
+    // we're modifying the existing ones, not creating new brushes.
+    // this can get a bit hairy if in future updates we add/remove/rename any of the brushes, or their parameters
+    for (var brushName in data.brushes) {
+        if (this.brushes[brushName]) {
+            this.brushes[brushName].parameters = data.brushes[brushName].parameters;
+        }
+    }
+};
