5223d7cd574877be2afeffa733368ac242e26054
Martijn <tijntje_7@msn.com>
2016-09-08 23:48:26 +0200

    Add serialize/deserialize methods to colorModel

--- source/components/color/colorModel.js
+++ source/components/color/colorModel.js
@@ -11,3 +11,15 @@ ColorModel.prototype.setColorFn = function (colorFn) {
 ColorModel.prototype.getColorFn = function () {
     return this.colorFn;
 };
+
+ColorModel.prototype.serialize = function () {
+    return {colorFn: this.colorFn.toString()};
+};
+
+ColorModel.prototype.loadFromSerialization = function (data) {
+    // the colorFn script defines the `color` function.
+    // eval`ing that script introduces tis color fn into our scope, allowing
+    // us to assign it to this.colorFn
+    eval(data.colorFn);
+    this.colorFn = color;
+};
